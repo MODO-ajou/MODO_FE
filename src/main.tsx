2fa926bffeef5d8357fd1509e3ebf508a8180b63
Jae Han Song <rfv1479@gmail.com>
2023-05-02 17:35:09 +0900

    MODO-17 feat : fallback 로직 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -8,8 +8,10 @@ import '@/styles/font.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Suspense fallback={<>로딩중...</>}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Suspense>
   </React.StrictMode>,
 );
