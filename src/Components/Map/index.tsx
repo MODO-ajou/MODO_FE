4bb776e87f2b826f1158a12f7feb1d104ec30dce
turfguy <ksm0811@ajou.ac.kr>
2023-05-20 23:37:12 +0900

    MODO-40 feat: 현재위치 마커 구현

--- src/Components/Map/index.tsx
+++ src/Components/Map/index.tsx
@@ -1,44 +1,76 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import { ReactComponent as Location } from '@/assets/location.svg';
 
+const { naver } = window;
 export default function Map() {
-  const [mapPoint, setMapPoint] = useState({ x: null, y: null });
-  const [location, setLocation] = useState('');
+  const [myLocation, setMyLocation] = useState<{ latitude: number; longitude: number } | string>(
+    '',
+  );
+
+  // get current position
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+    } else {
+      window.alert('현재위치를 알수 없습니다.');
+    }
+  }, []);
 
   useEffect(() => {
-    const mapDiv = document.getElementById('map');
-    const map = new window.naver.maps.Map(mapDiv);
-    window.naver.maps.Event.addDOMListener(mapDiv, 'click', () => {
-      const coordinate = { x: map.data.map.center.x, y: map.data.map.center.y };
-      setMapPoint({ x: coordinate.x, y: coordinate.y });
-      window.naver.maps.Service.reverseGeocode(
-        {
-          coords: new window.naver.maps.LatLng(coordinate.y, coordinate.x),
-          orders: [
-            window.naver.maps.Service.OrderType.ADDR,
-            window.naver.maps.Service.OrderType.ROAD_ADDR,
-          ].join(','),
-        },
-        (
-          status: number,
-          response: {
-            v2: {
-              address: {
-                jibunAddress: string;
-              };
-            };
-          },
-        ) => {
-          if (status !== window.naver.maps.Service.Status.OK) {
-            return alert('Something wrong!');
-          }
-          const result = response.v2;
-          setLocation(result.address.jibunAddress);
-        },
-      );
-    });
+    if (typeof myLocation !== 'string') {
+      const currentPosition = [myLocation.latitude, myLocation.longitude];
+
+      const map = new naver.maps.Map('map', {
+        center: new naver.maps.LatLng(currentPosition[0], currentPosition[1]),
+        zoomControl: true,
+      });
+    }
+  }, [myLocation]);
+  const [geoUrl, setGeoUrl] = useState(''); // open API를 호출할 때 쓰이는 url
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        setMyLocation({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+        setGeoUrl(
+          'https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=' +
+            `${position.coords.latitude},${position.coords.longitude}` +
+            '&orders=addr&output=json',
+        );
+      });
+    } else {
+      window.alert('현재위치를 알수 없습니다.');
+    }
   }, []);
+
+  useEffect(() => {
+    console.log('geoURL:::', geoUrl);
+    const getAddress = async () => {
+      axios
+        .request({
+          url: geoUrl,
+          method: 'GET',
+          headers: {
+            'X-NCP-APIGW-API-KEY-ID': import.meta.env.VITE_NMF_CLIENT_ID as string, // 앱 등록 시 발급받은 Client ID
+            'X-NCP-APIGW-API-KEY': import.meta.env.VITE_NMF_SECRET_CLIENT as string, // 앱 등록 시 발급받은 Client Secret
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+        });
+    };
+    getAddress();
+  });
   return (
     <div>
       <div
@@ -55,9 +87,6 @@ export default function Map() {
       >
         <Location className="absolute z-40" />
       </div>
-      <div>{mapPoint.x}</div>
-      <div>{mapPoint.y}</div>
-      <div>주소는 : {location}</div>
     </div>
   );
 }
